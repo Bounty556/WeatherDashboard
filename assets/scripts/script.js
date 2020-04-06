bc61c15bd772131c2861c8688206a82b5d529ba9
Bounty556 <jacobmayday@gmail.com>
2020-04-06 15:02:33 -0600

    Added functioning search history buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,24 +2,36 @@ var apiKey = 'dcbf84d1f01ba47709c6cfcfcb884b84';
 var searchHistory;
 
 $(document).ready(function() {
-    $('#search-btn').on('click', search);
-
     // Make an empty array if searchHistory doesn't exist
     searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
     populateSearchHistory();
+
+    $('#search-btn').on('click', function() {
+        search($('#search-input').val().trim());
+    });
+
+    $('#search-history').on('click', '.search-history-item', function() {
+        search($(this).attr('data-id'));
+    });
 });
 
-function search() {
-    let cityName = $('#search-input').val().trim();
+function search(query) {
+    let queryURL;
+
+    if (isNaN(parseInt(query))) { // Must be city name
+        queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + query;
+    } else { // Must be city ID
+        queryURL = 'https://api.openweathermap.org/data/2.5/weather?id=' + query;
+    }
 
     $.ajax({
-        url: 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey,
+        url: queryURL + '&units=imperial&appid=' + apiKey,
         method: 'GET'
     }).then(function(response) {
         console.log(response);
 
-        addSearchHistoryItem(response.name, response.id, response.coord.lat, response.coord.lon);
+        addSearchHistoryItem(response.name, response.id);
 
         $.ajax({
             url: 'https://api.openweathermap.org/data/2.5/onecall?lon=' + response.coord.lon + '&lat=' + response.coord.lat + '&units=imperial&appid=' + apiKey,
@@ -30,12 +42,10 @@ function search() {
     });
 }
 
-function addSearchHistoryItem(name, id, lat, lon) {
+function addSearchHistoryItem(name, id) {
     let historyItem = {
         name: name,
-        id: id,
-        lat: lat,
-        lon: lon
+        id: id
     };
     searchHistory.unshift(historyItem);
 
@@ -54,8 +64,6 @@ function populateSearchHistory() {
     searchHistory.forEach(element => {
         let p = $('<p>').attr('class', 'search-history-item');
         p.attr('data-id', element.id.toString());
-        p.attr('data-lat', element.lat.toString());
-        p.attr('data-lon', element.lon.toString());
 
         p.text(element.name);
 
